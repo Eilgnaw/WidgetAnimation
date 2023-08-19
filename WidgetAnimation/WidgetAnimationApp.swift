427fca49fb74b420aa4fb54687e092c64353d942
Eilgnaw <wzqwzq666@hotmail.com>
2023-08-19 15:19:51 +0800

    Initial Commit

---
+++ WidgetAnimation/WidgetAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetAnimationApp.swift
+//  WidgetAnimation
+//
+//  Created by 王小劣 on 2023/8/19.
+//
+
+import SwiftUI
+
+@main
+struct WidgetAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
